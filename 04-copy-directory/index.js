39813ae8f6b50279bd64b1b5934524d0d64129c5
Pavlkan <PavelKarman.Pavlkan@gmail.com>
2022-05-24 01:09:38 +0300

    Update README.md

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,6 @@
+// !Огромная просьба, проверьте мою работу в четверг!
+
+const fsp = require('fs/promises');
+const path = require('path');
+const pathToCopiedFiles = path.join(__dirname, 'files-copy');
 
